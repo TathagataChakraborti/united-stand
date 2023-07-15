a016798e0c4ffa934609f82ffb48693b88be9f58
TathagataChakraborti <tathagata.net@live.com>
2023-07-15 15:05:01 -0400

    meta data complete
    Signed-off-by: TathagataChakraborti <tathagata.net@live.com>

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,7 +2,21 @@ function generateImageUrl(imageUrl) {
     return `url(${process.env.PUBLIC_URL}/images/${imageUrl}.png)`;
 }
 
-function fixDate(date) {
+function getAverage(array) {
+    const sum = array.reduce((total, item) => total + item, 0);
+    return sum / array.length;
+}
+
+function prettyScore(score) {
+    return 'home home_score-away_score away'
+        .replace('home', score[0].team_name)
+        .replace('home_score', score[0].score)
+        .replace('away_score', score[1].score)
+        .replace('away', score[1].team_name);
+}
+
+function fixDate(date_string) {
+    const date = new Date(date_string);
     date.setDate(date.getDate() + 1);
     return date;
 }
@@ -26,8 +40,10 @@ function tournamentNames(data) {
 
 export {
     generateImageUrl,
+    getAverage,
+    prettyScore,
+    fixDate,
     getAllRatings,
     getAllFixtures,
     tournamentNames,
-    fixDate,
 };
--- src/content/MetaDataPage/MetaDataPage.js
+++ src/content/MetaDataPage/MetaDataPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { LineChart } from '@carbon/charts-react';
-import { tournamentNames, getAllRatings, fixDate } from '../../components/Info';
+import { LineChart, SimpleBarChart } from '@carbon/charts-react';
+import {
+    tournamentNames,
+    fixDate,
+    getAverage,
+    prettyScore,
+} from '../../components/Info';
 import {
     Grid,
     Column,
@@ -9,6 +14,11 @@ import {
     InlineNotification,
     AccordionItem,
     Accordion,
+    StructuredListWrapper,
+    StructuredListHead,
+    StructuredListBody,
+    StructuredListRow,
+    StructuredListCell,
 } from '@carbon/react';
 
 import '@carbon/charts-react/styles.css';
@@ -19,8 +29,8 @@ const selection_items = tournaments.map(item => {
     return { id: item };
 });
 
-const engagementOptions = {
-    title: 'Engagement statistics over time',
+const engagement_metrics = ['Views', 'Likes', 'Shares'];
+const plotOptions = {
     axes: {
         bottom: {
             mapsTo: 'date',
@@ -31,12 +41,48 @@ const engagementOptions = {
             scaleType: 'linear',
         },
     },
-    legend: { enabled: false },
     curve: 'curveMonotoneX',
     height: '400px',
 };
 
-const engagement_metrics = ['Views', 'Likes', 'Shares'];
+const getEngagementOptions = engagementType => {
+    return {
+        ...plotOptions,
+        title: 'Engagement Statistics for Season 2022-2023 | ' + engagementType,
+        legend: { enabled: false },
+    };
+};
+
+const getVotesOptions = e => {
+    return {
+        ...plotOptions,
+        title: 'Engagement Statistics Over Time',
+        axes: {
+            ...plotOptions.axes,
+            bottom: {
+                ...plotOptions.axes.bottom,
+                title: 'Number of Votes',
+            },
+        },
+    };
+};
+
+const getAggregateVotesOptions = e => {
+    return {
+        ...plotOptions,
+        title: 'Average Voting Statistics per Match Outcome',
+        axes: {
+            ...plotOptions.axes,
+            left: {
+                mapsTo: 'group',
+                scaleType: 'labels',
+            },
+            bottom: plotOptions.axes.left,
+        },
+        height: '200px',
+        legend: { enabled: false },
+    };
+};
 
 class MetaDataPage extends React.Component {
     constructor(props) {
@@ -53,40 +99,91 @@ class MetaDataPage extends React.Component {
         });
     }
 
-    getEngagementOptions({ engagementType }) {
-        return {
-            ...engagementOptions,
-            title: engagementOptions.title + ' | ' + engagementType,
-        };
-    }
-
-    getEngagementData({ engagementType }) {
+    inTournamentSelections(match_data) {
         const current_selections = this.state.current_selections.map(
             item => item.id
         );
-        const data2023 = data.season_data.find(
+
+        return current_selections.indexOf(match_data.meta_data.tournament) > -1;
+    }
+
+    filterDataByValidTournaments() {
+        return data.season_data.map(season_data => {
+            return {
+                ...season_data,
+                match_data: season_data.match_data.filter(match_data =>
+                    this.inTournamentSelections(match_data)
+                ),
+            };
+        });
+    }
+
+    getEngagementData({ engagementType }) {
+        const data_valid_tournaments = this.filterDataByValidTournaments(data);
+        const data2023 = data_valid_tournaments.find(
             item => item.season.end === 2023
         );
-        const data2023_valid_tournaments = data2023.match_data.filter(
-            item => current_selections.indexOf(item.meta_data.tournament) > -1
-        );
 
-        const filter_by_engagement_type = data2023_valid_tournaments.map(
-            item => {
-                const date = new Date(item.meta_data.date);
-
-                return {
-                    group: engagementType,
-                    date: fixDate(date),
-                    value:
-                        item.united_stand.meta_data[
-                            engagementType.toLowerCase()
-                        ],
-                };
-            }
-        );
+        const filter_by_engagement_type = data2023.match_data.map(item => {
+            return {
+                group: engagementType,
+                date: fixDate(item.meta_data.date),
+                value:
+                    item.united_stand.meta_data[engagementType.toLowerCase()],
+            };
+        });
 
         return filter_by_engagement_type;
+    }
+
+    getVotesData() {
+        const data_valid_tournaments = this.filterDataByValidTournaments(data);
+
+        const votes_data = data_valid_tournaments
+            .map(item => item.match_data)
+            .reduce((bag, item) => bag.concat(item), [])
+            .filter(
+                item => item.united_stand && item.united_stand.man_of_the_match
+            )
+            .map(item => {
+                const date = fixDate(item.meta_data.date);
+                const result = item.meta_data.result.split('.')[1];
+
+                return {
+                    group: result,
+                    date: date,
+                    value: item.united_stand.man_of_the_match.votes,
+                    meta_data: item.meta_data,
+                };
+            });
+
+        return votes_data;
+    }
+
+    getAggregateVotesData() {
+        const all_votes_data = this.getVotesData();
+        const categories = ['LOSS', 'DRAW', 'WIN'];
+
+        return categories.map(result => {
+            return {
+                group: result,
+                value: getAverage(
+                    all_votes_data
+                        .filter(v => v.group === result)
+                        .map(v => v.value)
+                ),
+            };
+        });
+    }
+
+    getTopVotesData() {
+        var all_votes_data = this.getVotesData();
+
+        all_votes_data.sort(function(a, b) {
+            return b.value - a.value;
+        });
+
+        return all_votes_data.slice(0, 7);
     }
 
     render() {
@@ -147,9 +244,9 @@ class MetaDataPage extends React.Component {
                                             data={this.getEngagementData({
                                                 engagementType: item,
                                             })}
-                                            options={this.getEngagementOptions({
-                                                engagementType: item,
-                                            })}></LineChart>
+                                            options={getEngagementOptions(
+                                                item
+                                            )}></LineChart>
                                         <br />
                                         <br />
                                     </AccordionItem>
@@ -172,7 +269,7 @@ class MetaDataPage extends React.Component {
                                     className="no-decoration-enforce text-red"
                                     href="https://www.youtube.com/watch?v=bp3pGkSvs1k"
                                     target="_blank">
-                                    Robbie of AFTV
+                                    Don Robbie of AFTV
                                 </Link>
                                 , and formerly failed football club chairman of
                                 Crystal Palace, Simon Jordan on the latter's
@@ -192,57 +289,147 @@ class MetaDataPage extends React.Component {
                                 positively correlates with the team doing well,
                                 and vastly overshadows reach to rival fans when
                                 the team does poorly. Here below, we can observe
-                                this phenomenon in numbers. Note that, we chose
-                                the number of votes cast for the Man of the
-                                Match as the proxy to the number of votes
-                                reported below since this is recorded at the end
-                                of the live match review (unless scraped after
-                                the fact from the page).
+                                this phenomenon in numbers.
+                                <br />
+                                <br />
+                                Note that, we chose the number of votes cast for
+                                the Man of the Match as the proxy to the number
+                                of votes reported below since this is recorded
+                                at the end of the live match review (unless
+                                scraped automatically after the fact from the
+                                page). Hover or click on the graph legends
+                                (W/D/L) below, and select one or more
+                                tournaments from the tournament selector above,
+                                to explore the data.
+                                <br />
+                                <br />
+                                <SimpleBarChart
+                                    data={this.getVotesData()}
+                                    options={getVotesOptions()}></SimpleBarChart>
                             </p>
+
+                            <br />
+                            <br />
+                            <Grid>
+                                <Column sm={4} md={6} lg={8}>
+                                    <SimpleBarChart
+                                        data={this.getAggregateVotesData()}
+                                        options={getAggregateVotesOptions()}></SimpleBarChart>
+
+                                    <br />
+                                    <br />
+
+                                    <InlineNotification
+                                        kind="info"
+                                        lowContrast
+                                        hideCloseButton
+                                        subtitle={
+                                            <div style={{ marginTop: '10px' }}>
+                                                We hope to add to this data with
+                                                further analysis of what factors
+                                                drive engagement and to what
+                                                degree (e.g. which tournament,
+                                                how many goals and when, and so
+                                                on).
+                                            </div>
+                                        }
+                                        timeout={0}
+                                        title="Variables and Externalities"
+                                    />
+
+                                    <br />
+                                    <br />
+
+                                    <InlineNotification
+                                        lowContrast
+                                        hideCloseButton
+                                        subtitle={
+                                            <div style={{ marginTop: '10px' }}>
+                                                Ideally, we would have liked to
+                                                draw these numbers with the
+                                                metrics of views, likes, and
+                                                shares (as seen above in the
+                                                first section) and not the
+                                                number of votes (cast for the
+                                                Man of the Match). However, the
+                                                TUS articles from previous
+                                                seasons no longer contain this
+                                                data.{' '}
+                                                <span
+                                                    role="img"
+                                                    aria-label="cry face">
+                                                    &#128557;
+                                                </span>{' '}
+                                                <span
+                                                    role="img"
+                                                    aria-label="cry face">
+                                                    &#128557;
+                                                </span>
+                                            </div>
+                                        }
+                                        timeout={0}
+                                        title="OLDER DATA WANTED"
+                                    />
+                                </Column>
+                                <Column sm={4} md={6} lg={8}>
+                                    <StructuredListWrapper ariaLabel="Structured list">
+                                        <StructuredListHead>
+                                            <StructuredListRow
+                                                head
+                                                tabIndex={0}>
+                                                <StructuredListCell head>
+                                                    Top 10 Results by Engagement
+                                                </StructuredListCell>
+                                                <StructuredListCell head>
+                                                    Tournament
+                                                </StructuredListCell>
+                                                <StructuredListCell head>
+                                                    Result
+                                                </StructuredListCell>
+                                                <StructuredListCell head>
+                                                    Votes
+                                                </StructuredListCell>
+                                            </StructuredListRow>
+                                        </StructuredListHead>
+                                        <StructuredListBody>
+                                            {this.getTopVotesData().map(
+                                                item => (
+                                                    <StructuredListRow
+                                                        tabIndex={0}>
+                                                        <StructuredListCell>
+                                                            {prettyScore(
+                                                                item.meta_data
+                                                                    .score
+                                                            )}
+                                                            <br />
+                                                            <span className="feedback">
+                                                                {fixDate(
+                                                                    item
+                                                                        .meta_data
+                                                                        .date
+                                                                ).toDateString()}
+                                                            </span>
+                                                        </StructuredListCell>
+                                                        <StructuredListCell>
+                                                            {
+                                                                item.meta_data
+                                                                    .tournament
+                                                            }
+                                                        </StructuredListCell>
+                                                        <StructuredListCell>
+                                                            {item.group}
+                                                        </StructuredListCell>
+                                                        <StructuredListCell>
+                                                            {item.value}
+                                                        </StructuredListCell>
+                                                    </StructuredListRow>
+                                                )
+                                            )}
+                                        </StructuredListBody>
+                                    </StructuredListWrapper>
+                                </Column>
+                            </Grid>
                         </div>
-
-                        <InlineNotification
-                            kind="info"
-                            lowContrast
-                            hideCloseButton
-                            subtitle={
-                                <div style={{ marginTop: '10px' }}>
-                                    We hope to add to this data with further
-                                    analysis of what factors drive engagement
-                                    and to what degree (e.g. which tournament,
-                                    how many goals and when, and so on).
-                                </div>
-                            }
-                            timeout={0}
-                            title="Variables and Externalities"
-                        />
-
-                        <br />
-                        <br />
-
-                        <InlineNotification
-                            lowContrast
-                            hideCloseButton
-                            subtitle={
-                                <div style={{ marginTop: '10px' }}>
-                                    Ideally, we would have liked to draw these
-                                    numbers with the metrics of views, likes,
-                                    and shares (as seen above in the first
-                                    section) and not the number of votes (cast
-                                    for the Man of the Match). However, the TUS
-                                    articles from previous seasons no longer
-                                    contain this data.{' '}
-                                    <span role="img" aria-label="cry face">
-                                        &#128557;
-                                    </span>{' '}
-                                    <span role="img" aria-label="cry face">
-                                        &#128557;
-                                    </span>
-                                </div>
-                            }
-                            timeout={0}
-                            title="OLDER DATA WANTED"
-                        />
                     </div>
                 </Column>
             </Grid>
